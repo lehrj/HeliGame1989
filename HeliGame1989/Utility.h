72b3f66786d9838b28049d746f8b546d80d90ee4
w.lehrjackson <w.lehrjackson@gmail.com>
2021-12-06 22:14:44 -0500

    Fixed error with angle calculation potentially giving back garbage data

--- HeliGame1989/Utility.h
+++ HeliGame1989/Utility.h
@@ -32,7 +32,32 @@ public:
     {
         aVec1.Normalize();
         aVec2.Normalize();
-        const float angle = acos(aVec1.Dot(aVec2));
+        float dot = aVec1.Dot(aVec2);
+        float testCos = acos(dot);
+        float angle = acos(aVec1.Dot(aVec2));
+
+        float step1 = dot / aVec1.Length();
+        float step2 = step1 / aVec2.Length();
+        float deg = Utility::ToDegrees(step2);
+
+        float testCos2 = acos(26.9682884);
+
+        DirectX::SimpleMath::Vector3 testAng = DirectX::XMVector3AngleBetweenNormals(aVec1, aVec2);
+        float testAngLength = testAng.Length();
+
+        if (dot > 1.0f)
+        {
+            dot = 1.0f;
+        }
+        angle = acos(dot);
+        if (angle < (Utility::GetPi() * 2.0f) || angle > (Utility::GetPi() * -2.0f))
+        {
+        }
+        else
+        {
+            int testBreak = 0;
+        }
+
         return angle;
     }
 
@@ -44,7 +69,18 @@ public:
 
         float angle = GetAngleBetweenVectors(aVec1, aVec2);
         DirectX::SimpleMath::Vector3 torqueVec2 = aVec1.Cross(aVec2) * sin(GetAngleBetweenVectors(aVec1, aVec2));
- 
+        
+        DirectX::SimpleMath::Vector3 testVec = torqueVec2;
+        testVec.Normalize();
+        if (testVec.Length() < 1.1f || testVec.Length() > 0.9f)
+        {
+        }
+        else
+        {
+            int testBreak = 0;
+        }
+
+
         return torqueVec2;
     }
     
@@ -67,12 +103,23 @@ public:
     static DirectX::SimpleMath::Matrix GetTorqueMat(const DirectX::SimpleMath::Vector3 aVec1, const DirectX::SimpleMath::Vector3 aVec2, const float aStepMod)
     {
         DirectX::SimpleMath::Vector3 torqueVec = GetTorqueVec(aVec1, aVec2);
-
+        torqueVec.Normalize();
         DirectX::SimpleMath::Matrix torqueMat = DirectX::SimpleMath::Matrix::Identity;
         if (torqueVec != DirectX::SimpleMath::Vector3::Zero)
         {
             torqueMat *= DirectX::SimpleMath::Matrix::CreateFromAxisAngle(torqueVec, torqueVec.Length() * aStepMod);
         }
+
+        DirectX::SimpleMath::Vector3 testUp = DirectX::SimpleMath::Vector3::UnitY;
+        testUp = DirectX::SimpleMath::Vector3::Transform(testUp, torqueMat);
+        if (testUp.Length() < 1.1f || testUp.Length() > 0.9f)
+        {
+        }
+        else
+        {
+            int testBreak = 0;
+        }
+
 
         return torqueMat;
     }
